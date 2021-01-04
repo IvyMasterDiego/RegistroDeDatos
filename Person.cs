d293e876ce58b654143a32213865b9dc74bc7b7a
Diego Lobato <45220823+IvyMasterDiego@users.noreply.github.com>
2021-01-04 00:57:55 -0400

    Actualizando y agregando constructores

--- Person.cs
+++ Person.cs
@@ -1,4 +1,4 @@
-﻿using LabWork;
+using LabWork;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -12,24 +12,29 @@ namespace OOP
 
         public string Id { get; }
         public string Name { get; }
-        private int _packetData = 0;
+
         public double Ahorros;
         public string Password { get; set; }
-        public int Age => (_packetData >> 4);
+        private int bitpacking = 0;
+        public int Age => (bitpacking >> 4);
+        public Gender gender => (Gender)(bitpacking & 0b1000);
+        public MStatus mStatus => (MStatus)(bitpacking & 0b100);
+        public AGrad aGrad => (AGrad)(bitpacking & 0b11);
 
-        public Person(in string id, in string name,in int age,in double ahorros,in string password)
+
+        public Person(in string id, in string name, in int age, in double ahorros, in Gender gender, in MStatus mStatus, in AGrad aGrad, in string password)
         {
             Id = id;
             Name = name;
-            Password = password;
             Ahorros = ahorros;
-            _packetData = (age << 4);
+            Password = password;
+            bitpacking = (age << 4) | (int)gender | (int)mStatus | (int)aGrad;
         }
-        public override string ToString() => $"{GetType().Name}({nameof(Id)}:{Id};{nameof(Name)}:{Name};{nameof(_packetData)}:{_packetData};{nameof(Age)}:{Age}; {nameof(Ahorros)}:{Ahorros};{nameof(Password)}:{Password};)";
+        public override string ToString() => $"{GetType().Name}({nameof(Id)}:{Id}; {nameof(Name)}:{Name}; {nameof(bitpacking)}:{bitpacking}; {nameof(Age)}:{Age}; {nameof(Gender)}:{gender}; {nameof(MStatus)}:{mStatus}; {nameof(AGrad)}:{aGrad}; {nameof(Ahorros)}:{Ahorros}; {nameof(Password)}:{Password};)";
 
         public override bool Equals(object obj)
         {
-           if(obj is Person other)
+            if (obj is Person other)
             {
                 return Id.Equals(other.Id);
             }
@@ -41,19 +46,22 @@ namespace OOP
             string[] tokens = line.Split(',');
             (string id,
                 string name,
-                string password,
                 double ahorros,
-                int packetData
+              int bitpacking,
+                string password
                 ) =
                 (
                 tokens[0],
                 tokens[1],
-                tokens[2],
-                double.Parse(tokens[3]),
-                int.Parse(tokens[4])
+                double.Parse(tokens[2]),
+                int.Parse(tokens[3]),
+                tokens[4]
                 );
-            int age = (packetData >> 4);
-            return new Person(id, name, age,ahorros,password);
+            int age = (bitpacking >> 4);
+            Gender gender = (Gender)(bitpacking & 0b1000);
+            MStatus mStatus = (MStatus)(bitpacking & 0b100);
+            AGrad aGrad = (AGrad)(bitpacking & 0b11);
+            return new Person(id, name, age, ahorros, gender, mStatus, aGrad, password);
         }
 
         internal static Person FromConsole(string record)
@@ -61,28 +69,34 @@ namespace OOP
             var tokens = record.Split(',');
             (string id,
                 string name,
-                string password,
+                int age,
                 double ahorros,
-                int age
+                string password,
+                Gender gender,
+                MStatus mStatus,
+                AGrad aGrad
                 ) =
                 (
                 tokens[0],
                 tokens[1],
-                tokens[2],
+                int.Parse(tokens[2]),
                 double.Parse(tokens[3]),
-                int.Parse(tokens[4])
+                tokens[4],
+                (Gender)int.Parse(tokens[5]),
+            (MStatus)int.Parse(tokens[6]),
+            (AGrad)int.Parse(tokens[7])
                 );
-            return new Person(id, name, age,ahorros,password);
+            return new Person(id, name, age, ahorros, gender, mStatus, aGrad, password);
         }
 
         internal static void SaveToCsv()
         {
             if (persons.Count() > 0)
             {
-                File.WriteAllText(Program.file, "Cedula,Nombre,Edad,Ahorros,Password");
-                foreach(var p in Person.persons)
+                File.WriteAllText(Program.file, "Cedula,Nombre,Ahorros,Password,Data");
+                foreach (var p in Person.persons)
                 {
-                    File.AppendAllText(Program.file, $"{Environment.NewLine}{p.Id},{p.Name},{p._packetData},{p.Ahorros},{p.Password}");
+                    File.AppendAllText(Program.file, $"{Environment.NewLine}{p.Id},{p.Name},,{p.Ahorros},{p.Password},{p.Age}");
                 }
             }
         }
@@ -125,5 +139,22 @@ namespace OOP
             var eras = persons.RemoveAll(x => x.Id.Trim() == id.Trim());
             return eras;
         }
+    }
+    public enum Gender
+    {
+        Male = 0,
+        Female = 8
+    }
+    public enum MStatus
+    {
+        Single = 0,
+        Married = 4
+    }
+    public enum AGrad
+    {
+        Inicial = 0,
+        Bachiller = 1,
+        Grado = 2,
+        PostGrado = 3
     }
 }
